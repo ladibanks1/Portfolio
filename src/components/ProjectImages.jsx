2e3deb008de6d32a0d85c0cf6f28c1779cf7f11b
Oladipupo <oladipupobankole24@gmail.com>
2024-10-13 08:06:57 +0100

    added a target attribute to the projectImages Component

--- src/components/ProjectImages.jsx
+++ src/components/ProjectImages.jsx
@@ -10,34 +10,36 @@ const ProjectImages = ({
   img3,
 }) => {
   return (
-      <div className="w-full !p-2 md:!p-5 lg:!p-10 sm:w-1/2">
-        {/* Projects */}
-        <div className="p-2">
-          <Carousel img1={img1} img2={img2} img3={img3} />
-        </div>
-        <div>
-          <h2 className="mt-2">{title}</h2>
-          <ul className="flex w-full gap-2 text-slate-300 text-sm">
-            {tools.map((tool, index) => (
-              <li key={index}>{tool}</li>
-            ))}
-          </ul>
-          <section className="flex justify-between mt-5">
-            <a
-              href={projectLink}
-              className="border-b-[3px] border-green-400  text-md"
-            >
-              VIEW PROJECT
-            </a>
-            <a
-              href={codeLink}
-              className="border-b-[3px] border-green-400  text-md"
-            >
-              VIEW CODE
-            </a>
-          </section>
-        </div>
+    <div className="w-full !p-2 md:!p-5 lg:!p-10 sm:w-1/2">
+      {/* Projects */}
+      <div className="p-2">
+        <Carousel img1={img1} img2={img2} img3={img3} />
       </div>
+      <div>
+        <h2 className="mt-2">{title}</h2>
+        <ul className="flex w-full gap-2 text-slate-300 text-sm">
+          {tools.map((tool, index) => (
+            <li key={index}>{tool}</li>
+          ))}
+        </ul>
+        <section className="flex justify-between mt-5">
+          <a
+            target="_blank"
+            href={projectLink}
+            className="border-b-[3px] border-green-400  text-md"
+          >
+            VIEW PROJECT
+          </a>
+          <a
+            target="_blank"
+            href={codeLink}
+            className="border-b-[3px] border-green-400  text-md"
+          >
+            VIEW CODE
+          </a>
+        </section>
+      </div>
+    </div>
   );
 };
 
